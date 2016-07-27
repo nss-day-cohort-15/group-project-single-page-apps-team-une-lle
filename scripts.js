c4fc6629a8e3cbb2c1f47357a7069d7be0eee9c4
Elizabeth Sanger <libsang@gmail.com>
2016-07-27 14:48:29 -0500

    added event listeners to input field and clear button

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var inputField = document.getElementById("enterMessage");
+inputField.addEventListener("keypress", function(e) {
+    if (event.keyCode === 13) {
+        var userMessage = document.getElementById("enterMessage").value;
+        var outputField = document.getElementById("output field");
+        outputField.value = userMessage;
+    }
+})
+
+
+
+var clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", function(e) {
+document.getElementById("outputField").innerHTML = '';
+})
+
