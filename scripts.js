43a2f5d82dd517ca6183f75cf07c49f03b001d91
Elizabeth Sanger <libsang@gmail.com>
2016-07-28 13:00:33 -0500

    merging into master

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
+var messagesLogged = [];
+
 var inputField = document.querySelector("#enterMessage");
 inputField.addEventListener("keypress", function(e) {
     if (event.keyCode === 13) {
         var userMessage = document.querySelector("#enterMessage").value;
         console.log(userMessage);
         var outputField = document.querySelector(".outputField");
-        outputField.innerHTML += `<span>${userMessage}</span><button class="delete">Delete</button><br>`
-
+        outputField.innerHTML += `<div id="message-0"><span>${userMessage}</span><button class="delete">Delete</button></div>`
+        messagesLogged.push(userMessage)
+        console.log(messagesLogged)
         console.log('ya')
     }
+})
+
+document.querySelector("body").addEventListener("click", function(event) {
+  console.log(event);
+  if (event.target.tagName.toLowerCase() ===
+    "button") {
+    console.log('yes');
+  }
 })
 
 
