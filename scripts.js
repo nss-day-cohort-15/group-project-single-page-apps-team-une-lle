f76faf9591879d3f93f7b3a9172b6bc6f06b56bd
Elliott <ewilliamswtn@gmail.com>
2016-07-28 10:52:09 -0500

    working on checkbox js

--- scripts.js
+++ scripts.js
@@ -15,7 +15,27 @@ var clearButton = document.getElementById("clearButton");
 clearButton.addEventListener("click", function(e) {
     console.log('ta');
 document.querySelector(".outputField").innerHTML = '';
+
 })
+
+document.getElementById("darkThemeBox").addEventListener("click", function () {
+  if (document.getElementById("darkThemeBox").checked === true) {
+
+    document.getElementById("navbar").className = "navbar navbar-default navbar-fixed-top navbarDark";
+    document.getElementById("body").className = "bodyDark";
+    document.getElementById("enterMessage").className = "enterMessage enterMessageDark";
+    document.getElementById("clearButton").style.color = "black";
+
+  } else {
+
+    document.getElementById("navbar").className = "navbar navbar-default navbar-fixed-top";
+    document.getElementById("body").className = "";
+    document.getElementById("enterMessage").className = "enterMessage";
+    document.getElementById("clearButton").style.color = "white";
+
+  }
+});
+
 
 
 
