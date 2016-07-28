2cf02f6f082f70e870ddc9a0ac515c7f815cb406
Elizabeth Sanger <libsang@gmail.com>
2016-07-28 14:06:31 -0500

    tiny progress on delete button removing message

--- scripts.js
+++ scripts.js
@@ -5,39 +5,28 @@ inputField.addEventListener("keypress", function(e) {
     if (event.keyCode === 13) {
         var userMessage = document.querySelector("#enterMessage").value;
         console.log(userMessage);
-        var outputField = document.querySelector(".outputField");
-        outputField.innerHTML += `<div id="message-0"><span>${userMessage}</span><button class="delete">Delete</button></div>`
         messagesLogged.push(userMessage)
-        console.log(messagesLogged)
-        console.log('ya')
+        var outputField = document.querySelector(".outputField");
+        outputField.innerHTML += `<div id="outputDiv"><span id="outputSpan">${userMessage}</span><button class="delete">Delete</button></div>`
+
+        // console.log(messagesLogged)
+
     }
 })
 
 document.querySelector("body").addEventListener("click", function(event) {
-  console.log(event);
-  if (event.target.tagName.toLowerCase() ===
-    "button") {
-    console.log('yes');
+
+  if (event.target.className.toLowerCase() ===
+    "delete") {
+
+  var child = document.getElementById("outputSpan");
+  var parent = document.getElementById("outputDiv");
+  parent.removeChild(child);
+
+  console.log(messagesLogged);
   }
 })
 
-  // Handle the click event on any li
-  // if (event.target.tagName.toLowerCase() === "li") {
-  //   console.log("You clicked on an <li> element");
-  // }
-
-// var inputField = document.querySelector("#enterMessage");
-// inputField.addEventListener("keypress", function(e) {
-//     if (event.keyCode === 13) {
-//         var userMessage = document.querySelector("#enterMessage").value;
-//         console.log(userMessage);
-//         var outputField = document.querySelector(".outputField");
-//         outputField.innerHTML += `<div id="message-0"><span>${userMessage}</span><button class="delete">Delete</button></div>`
-
-
-//         console.log('ya')
-//     }
-// })
 
 var clearButton = document.getElementById("clearButton");
 clearButton.addEventListener("click", function(e) {
@@ -46,10 +35,6 @@ document.querySelector(".outputField").innerHTML = '';
 
 })
 
-// function listenPlease() {
-// document.querySelector("body").addEventListener("click", function(event) {
-//   console.log(event);
-// })
 
 document.getElementById("darkThemeBox").addEventListener("click", function () {
   if (document.getElementById("darkThemeBox").checked === true) {
